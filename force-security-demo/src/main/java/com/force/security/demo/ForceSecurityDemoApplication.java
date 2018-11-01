046ad2bfb21fb76a130476acf1bbe0a0fd1e909f
ym <ym@DESKTOP-0T5C339>
2018-11-01 20:08:15 +0800

    初始化导入安全模块

---
+++ force-security-demo/src/main/java/com/force/security/demo/ForceSecurityDemoApplication.java
@@ -0,0 +1,19 @@
+package com.force.security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ForceSecurityDemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ForceSecurityDemoApplication.class, args);
+	}
+
+	@GetMapping("/test")
+	public String test() {
+		return "测试";
+	}
+}
