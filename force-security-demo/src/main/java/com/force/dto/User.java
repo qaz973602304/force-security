450dc6a3d3d1885fe4c91be8ec5771482b2602a0
ym <ym@DESKTOP-0T5C339>
2018-11-02 18:28:23 +0800

    查询功能

---
+++ force-security-demo/src/main/java/com/force/dto/User.java
@@ -0,0 +1,30 @@
+package com.force.dto;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class User {
+	private String userName;
+	private String password;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword( String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userName=" + userName + ", password=" + password + "]";
+	}
+
+}
---
+++ force-security-demo/src/main/java/com/force/web/UserController.java
@@ -0,0 +1,28 @@
+package com.force.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.force.dto.User;
+import com.force.dto.UserQueryCondition;
+
+@RestController
+public class UserController {
+	@RequestMapping(name = "/user", method = RequestMethod.GET)
+	public List<User> query(UserQueryCondition userQueryCondition,@PageableDefault(page = 2,size = 17,sort = "userName,asc") Pageable pageable) {
+		List<User> userlist = new ArrayList<User>();
+		System.out.println(ReflectionToStringBuilder.toString(userQueryCondition,ToStringStyle.MULTI_LINE_STYLE));
+		userlist.add(new User());
+		userlist.add(new User());
+		userlist.add(new User());
+		return userlist;
+	}
+}
